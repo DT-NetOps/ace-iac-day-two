0ed87dbe53db444134bd5e0e7fc77794db3c3b3b
Dariusz Terefenko <dariusz.terefenko@gmail.com>
2022-05-05 19:27:13 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
-      organization = "DTConsulting"
+    organization = "DTConsulting"
     workspaces {
       name = "ace-iac-day-two"
     }
